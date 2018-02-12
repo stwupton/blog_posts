0169938d2a8410a71d90412f81bd961eefe5ddca
Steven Upton <supton.sz@gmail.com>
2018-02-12 17:18:35 +0000

    Fixed regex in extracting snippet.

--- bin/src/util.dart
+++ bin/src/util.dart
@@ -11,7 +11,7 @@ String get rootPath => Directory.current.uri.toFilePath();
 String extractSnippet(String markdown) {
   String html = markdownToHtml(markdown);
   String snippet = new RegExp('<p>(.+)</p>').firstMatch(html).group(1);
-  snippet = snippet.replaceAll(new RegExp(r'<\/?[a-zA-z\s\=\.\"\-\:\_\/1-9]+>'), '');
+  snippet = snippet.replaceAll(new RegExp(r'<\/?[a-zA-z\s\=\.\"\-\:\_\/0-9]+>'), '');
   snippet = snippet.length > 280
       ? snippet.replaceRange(280, snippet.length, '...')
       : snippet;
